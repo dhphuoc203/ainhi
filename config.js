b57bb54c2fa23b87ec165ce8a22c266b1675c7ec
dhphuoc203 <85792414+dhphuoc203@users.noreply.github.com>
2021-08-13 11:30:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,15 @@ const CONFIG = {
     Lạc vào tim tôi lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng bạn đã nghĩ tôi rất đẹp trai 😙',
+    title: 'Phải chăng bạn đã nghĩ tôi rất xinh gái phải không 😙',
     desc: 'Phải chăng bạn đã say tôi ngay từ lúc thấy nụ cười ấy, Haha ',
-    btnYes: 'Vẫn cứ là đẹp trai <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người, tôi là đẹp trai nhất :v',
+    btnYes: 'Ừm , bạn rất xinh',
+    btnNo: 'Không, bạn ảo à :3',
+    question:'Trên thế giới hơn 7 tỉ người, tôi là xinh gái nhất :v',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'bạn rất đẹp trai',
+    reply: 'Ok bạn rất là xinh :))',
     mess: 'Tôi biết mà 🥰. hihi 😘😘',
-    messDesc: 'Tối nay 7h .',
+    messDesc: 'Tối nay 7h tôi sẽ nhắn tin cho bạn .',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com/iamdanghoangphuoc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com/100017350473908' //link mess của các bạn. VD: https://m.me/iamdanghoangphuoc
 }
