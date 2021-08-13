a36e17b6d0a2c565e1bbe4c6ca8468bd99f992c0
dhphuoc203 <85792414+dhphuoc203@users.noreply.github.com>
2021-08-13 11:26:24 +0700

    Initial commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Bạn ơi!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào tim tôi lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng bạn đã nghĩ tôi rất đẹp trai 😙',
+    desc: 'Phải chăng bạn đã say tôi ngay từ lúc thấy nụ cười ấy, Haha ',
+    btnYes: 'Vẫn cứ là đẹp trai <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người, tôi là đẹp trai nhất :v',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'bạn rất đẹp trai',
+    mess: 'Tôi biết mà 🥰. hihi 😘😘',
+    messDesc: 'Tối nay 7h .',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/iamdanghoangphuoc' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
